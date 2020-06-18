77531d394464e3be0835bca6a501342ca6b48f70
Ryan Holm <ryan.j.holm@gmail.com>
2020-06-17 23:14:53 -0600

    starting

---
+++ gamer.js
@@ -0,0 +1,12 @@
+const first = document.getElementById("qwerty");
+const second = document.getElementById("phrase");
+const button = document.getElementById("btn-reset");
+
+const missed;
+for (i = 0; i < missed.length; i++) {
+}
+
+
+
+
+
