1a1605d5c5ebc9e8b81521f3f2bea1a03273315a
Ryan Holm <ryan.j.holm@gmail.com>
2020-07-09 21:28:40 -0600

    Simple adds

---
+++ app.js
@@ -0,0 +1,68 @@
+const first = document.getElementById("qwerty");
+const second = document.getElementById("phrase");
+const button = document.getElementById("btn-reset");
+
+
+let missed = 0;
+
+// Declare and initialize the phrases array, story at least five strings that
+//contain only letters and spaces, no punctuation
+
+let phrases = [
+    'first',
+    'second',
+    'third',
+    'fourth',
+    'fifth'
+];
+
+button.addEventListener('click') {
+    button.style.display = 'none'; //not sure if this is the right command
+}
+
+function getRandomPhraseAsArray(arr) {
+
+}
+
+getRandomPhraseAsArray(phrases);
+
+function addPhraseToDisplay(arr){
+    //do stuff any arr that is passed in, and add to `#phrase ul`
+}
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
+
+//7
+function checkLetter('click') {
+    if ()
+
+    return null;
+}
+
+
+//8 Event Listener
+
+
+
+//9
+
+
+//10
+
+
+
+
+
+//     let items = [];
+//     for (let i = 0; i < arr.length; i++){
+//         items += `<li>${ arr[i] }</li>`;
+//     }
+//     return items;
+// }
+
+
+
+
+
+
